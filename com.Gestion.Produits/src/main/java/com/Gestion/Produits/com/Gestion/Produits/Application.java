858f409055676411c2647a4fbfc88dd1ed9c808b
amal <76752777+amalblip@users.noreply.github.com>
2023-02-03 15:19:10 +0100

    first commit

---
+++ com.Gestion.Produits/src/main/java/com/Gestion/Produits/com/Gestion/Produits/Application.java
@@ -0,0 +1,13 @@
+package com.Gestion.Produits.com.Gestion.Produits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
